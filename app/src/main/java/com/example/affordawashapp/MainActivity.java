09d43a1211a88147207031cfabcf81da9fd7b9e0
Lewis Masallo <lewismasallo@gmail.com>
2022-12-01 08:36:04 +0800

    Cleaning

--- app/src/main/java/com/example/affordawashapp/MainActivity.java
+++ app/src/main/java/com/example/affordawashapp/MainActivity.java
@@ -2,17 +2,8 @@ package com.example.affordawashapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
