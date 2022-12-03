1a6edd4304b437bc5e33fb3bc077f405028480ae
wyncalim <119503023+wyncalim@users.noreply.github.com>
2022-12-03 11:03:11 +0800

    Update MainActivity.java
    Modified from Github

--- app/src/main/java/com/example/affordawashapp/MainActivity.java
+++ app/src/main/java/com/example/affordawashapp/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-
+//ITO YUNNG EDIT Ko
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper databaseHelper;
